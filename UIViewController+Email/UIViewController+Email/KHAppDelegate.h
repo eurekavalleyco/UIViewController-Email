a515410c3b3ef1d61b7eb68337a8a5bd0637a998
Ken M. Haggerty <eurekavalleyco@gmail.com>
2014-03-28 16:03:48 -0400

    Added LICENSE notice to all files.

--- UIViewController+Email/UIViewController+Email/KHAppDelegate.h
+++ UIViewController+Email/UIViewController+Email/KHAppDelegate.h
@@ -5,6 +5,9 @@
 //  Created by Ken M. Haggerty on 3/28/14.
 //  Copyright (c) 2014 Ken M. Haggerty. All rights reserved.
 //
+//  For full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
+//
 
 #pragma mark - // NOTES (Public) //
 
